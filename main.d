6d2629683004c6fb6ac14e15d4f3ee2e373c7ccc
gemmap520 <gemmap520@gmail.com>
2024-07-12 14:55:37 -0400

    Initial commit of Tic-Tac-Toe game with only source files

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cc tictactoe.h window.h /opt/X11/include/X11/Xlib.h \
-  /opt/X11/include/X11/X.h /opt/X11/include/X11/Xfuncproto.h \
-  /opt/X11/include/X11/Xosdefs.h
--- tictactoe.d
+++
@@ -1,3 +0,0 @@
-tictactoe.o: tictactoe.cc tictactoe.h window.h \
-  /opt/X11/include/X11/Xlib.h /opt/X11/include/X11/X.h \
-  /opt/X11/include/X11/Xfuncproto.h /opt/X11/include/X11/Xosdefs.h
